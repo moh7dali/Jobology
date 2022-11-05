4881343263f2996281a201bca35ded3e7ae5a58d
unknown <you@example.com>
2022-11-05 16:09:30 +0200

    update

--- lib/Screens/start.dart
+++ lib/Screens/start.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:get/get.dart';
 
 class Start_page extends StatefulWidget {
   const Start_page({super.key});
