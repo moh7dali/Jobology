82e34c51ec43bf2d0de79b46d1a270f0935d09bb
raghadromaneh <raghadromani22@gmail.com>
2022-11-06 23:29:56 +0200

    raghad update

--- lib/Screens/jobs.dart
+++ lib/Screens/jobs.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jobology/Screens/buttonnav.dart';
@@ -11,19 +12,71 @@ class jobs extends StatefulWidget {
   State<jobs> createState() => _jobsState();
 }
 
+String username = "";
+
 class _jobsState extends State<jobs> {
   @override
   Widget build(BuildContext context) {
+    FirebaseFirestore.instance
+        .collection('Users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .snapshots()
+        .listen((event) {
+      setState(() {
+        username = event['Fullname'];
+      });
+    });
     return Scaffold(
         appBar: AppBar(
+          toolbarHeight: 75,
+          backgroundColor: const Color.fromARGB(0, 255, 255, 255),
+          elevation: 0,
+          leading: Builder(
+            builder: (context) => IconButton(
+              icon: Image.asset(
+                "images/back.png",
+                width: 26,
+              ),
+              onPressed: () async {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Welcome",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 153, 152, 152),
+                      fontSize: 15,
+                    ),
+                  ),
+                  Text(
+                    username,
+                    style: TextStyle(color: Colors.black),
+                  )
+                ],
+              ),
+            ],
+          ),
+          centerTitle: true,
           actions: [
-            IconButton(
-                icon: const Icon(Icons.lightbulb),
-                onPressed: () {
-                  Get.isDarkMode
-                      ? Get.changeTheme(ThemeData.light())
-                      : Get.changeTheme(ThemeData.dark());
-                })
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, "personalPage");
+              },
+              child: Image.asset(
+                "images/user.png",
+                width: 50,
+              ),
+            ),
+            SizedBox(
+              width: 15,
+            ),
           ],
         ),
         body: StreamBuilder(
