5792757724a6be71ef12dcb9e1f3731a545cecef
unknown <you@example.com>
2022-11-06 18:34:35 +0200

    update

--- lib/Screens/course.dart
+++ lib/Screens/course.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import '../Widgets/courseWidget.dart';
 import '../Widgets/mytext.dart';
 
