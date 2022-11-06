91418acbaeba1e0fe5c95b3797739ff0535ea4d2
unknown <you@example.com>
2022-11-06 22:22:28 +0200

    update

--- lib/Screens/Users/Home.dart
+++ lib/Screens/Users/Home.dart
@@ -42,31 +42,6 @@ class _HomeState extends State<Home> {
         username = event['Fullname'];
       });
     });
-<<<<<<< HEAD
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 75,
-          backgroundColor: const Color.fromARGB(0, 255, 255, 255),
-          elevation: 0,
-          leading: Builder(
-            builder: (context) => IconButton(
-              icon: Image.asset(
-                "images/back.png",
-                width: 26,
-              ),
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return Login();
-                    },
-                  ),
-                );
-              },
-=======
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 75,
@@ -77,22 +52,19 @@ class _HomeState extends State<Home> {
             icon: Image.asset(
               "images/back.png",
               width: 26,
->>>>>>> 05f835c7fac7d4fbe48adf27519f05dbca124930
             ),
             onPressed: () {
               Navigator.pop(context);
             },
           ),
-<<<<<<< HEAD
-          title: Row(
-=======
         ),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GestureDetector(onTap: () {
-              Navigator.pushNamed(context, "personalPage");
-            },
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, "personalPage");
+              },
               child: Image.asset(
                 "images/user.png",
                 width: 50,
@@ -166,7 +138,6 @@ class _HomeState extends State<Home> {
         scrollDirection: Axis.vertical,
         children: [
           Row(
->>>>>>> 05f835c7fac7d4fbe48adf27519f05dbca124930
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Column(
@@ -252,191 +223,7 @@ class _HomeState extends State<Home> {
               ),
             ],
           ),
-<<<<<<< HEAD
-          centerTitle: true,
-          actions: [
-            Image.asset(
-              "images/user.png",
-              width: 50,
-            ),
-            SizedBox(
-              width: 15,
-            ),
-            // Row(
-            //   children: [
-            //     Column(
-            //       children: [
-            //         const SizedBox(
-            //           height: 15,
-            //         ),
-            //         FlutterSwitch(
-            //           value: status8,
-            //           activeColor: Color.fromARGB(135, 16, 153, 130),
-            //           activeIcon: const Icon(
-            //             Icons.dark_mode,
-            //             color: Color.fromARGB(255, 118, 122, 121),
-            //           ),
-            //           inactiveIcon: const Icon(
-            //             Icons.light_mode,
-            //             color: Color.fromARGB(255, 223, 239, 3),
-            //           ),
-            //           onToggle: (val) {
-            //             setState(() {
-            //               status8 = val;
-            //               if (Get.isDarkMode) {
-            //                 Get.changeTheme(ThemeData.light());
-            //               } else {
-            //                 Get.changeTheme(
-            //                   ThemeData.dark(),
-            //                 );
-            //               }
-            //             });
-            //           },
-            //         ),
-            //       ],
-            //     ),
-            //     const SizedBox(
-            //       width: 15,
-            //     ),
-            //   ],
-            // ),
-          ],
-        ),
-        body: ListView(
-          shrinkWrap: true,
-          scrollDirection: Axis.vertical,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    const Text(
-                      "Start your\ntrip,",
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    SectionsCard(
-                      OnTapping: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return Course();
-                            },
-                          ),
-                        );
-                      },
-                      CardTitle: "Courses",
-                      CardSubTitle: "Your way to your job",
-                      ImageName: "images/Courses.png",
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    SectionsCard(
-                      OnTapping: () {},
-                      CardTitle: "CV",
-                      CardSubTitle: "Create your CV",
-                      ImageName: "images/cv.png",
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  width: 30,
-                ),
-                Column(
-                  children: [
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    SectionsCard(
-                      OnTapping: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return jobs();
-                            },
-                          ),
-                        );
-                      },
-                      CardTitle: "Jobs",
-                      CardSubTitle: "Find your job easly",
-                      ImageName: "images/job.png",
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    SectionsCard(
-                      OnTapping: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return Interviwe_Preview();
-                            },
-                          ),
-                        );
-                      },
-                      CardTitle: "Interview Questions",
-                      CardSubTitle: "",
-                      ImageName: "images/interview.png",
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-        // bottomNavigationBar: Button(),
-        floatingActionButton: SpeedDial(
-            buttonSize: Size(70, 70),
-            spaceBetweenChildren: 15,
-            child: Icon(
-              Ionicons.menu,
-              size: 30,
-            ),
-            backgroundColor: Color.fromARGB(255, 61, 14, 70),
-            children: [
-              SpeedDialChild(
-                child: Icon(Icons.logout),
-                label: 'Logout',
-                onTap: () async {
-                  await FirebaseAuth.instance.signOut();
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return Login();
-                      },
-                    ),
-                  );
-                },
-              ),
-              SpeedDialChild(
-                child: Icon(Ionicons.person),
-                label: 'Profile',
-                onTap: () {
-                  Navigator.pushNamed(context, "personalPage");
-                },
-              ),
-              SpeedDialChild(
-                child: Icon(Ionicons.home),
-                label: 'Home',
-                onTap: () {
-                  Navigator.pushNamed(context, "Home");
-                },
-              ),
-            ]),
-=======
         ],
->>>>>>> 05f835c7fac7d4fbe48adf27519f05dbca124930
       ),
       bottomNavigationBar: Button(),
     );
