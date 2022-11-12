aff089438eb5f41b97c1eda6de00b6eb4f3c50dd
unknown <you@example.com>
2022-11-12 17:07:05 +0200

    update

--- lib/Screens/Company/update.dart
+++ lib/Screens/Company/update.dart
@@ -47,26 +47,17 @@ class _Update_jobState extends State<Update_job> {
 
   @override
   Widget build(BuildContext context) {
-    FirebaseFirestore.instance
-        .collection('Users')
-        .doc(FirebaseAuth.instance.currentUser!.uid)
-        .snapshots()
-        .listen((event) {
-      setState(() {
-        comp_name = event['Fullname'];
-        img_url = event['img'];
-      });
-    });
     TextEditingController titleController =
         TextEditingController(text: widget.title == null ? null : widget.title);
     TextEditingController briefController =
         TextEditingController(text: widget.breif == null ? null : widget.breif);
-    TextEditingController yearController =
+    TextEditingController yearsController =
         TextEditingController(text: widget.years == null ? null : widget.years);
     TextEditingController reqController =
         TextEditingController(text: widget.req == null ? null : widget.req);
     TextEditingController urlController =
         TextEditingController(text: widget.url == null ? null : widget.url);
+
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 75,
@@ -79,182 +70,147 @@ class _Update_jobState extends State<Update_job> {
                 width: 26,
               ),
               onPressed: () {
-                Navigator.pushNamed(context, "companyHome");
+                Navigator.pop(context);
               }),
         ),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Welcome",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 153, 152, 152),
-                    fontSize: subTitleSize,
-                  ),
-                ),
-                Text(
-                  "$comp_name ,",
-                  style: const TextStyle(color: Colors.black),
-                )
-              ],
-            ),
-          ],
-        ),
-        centerTitle: true,
-        actions: [
-          CircleAvatar(
-            radius: 25,
-            backgroundColor: Colors.grey,
-            backgroundImage: NetworkImage(img_url),
-          ),
-          const SizedBox(
-            width: 15,
-          ),
-        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Form(
-          child: ListView(
-            children: [
-              CircleAvatar(
-                  radius: 60,
-                  backgroundColor: Colors.grey,
-                  backgroundImage:
-                      pickedimg == null ? null : FileImage(pickedimg!)),
-              const SizedBox(
-                height: 10,
-              ),
-              Column(
-                children: [
-                  Text("Choose image From ?",
-                      style: GoogleFonts.poppins(
-                        fontSize: 16,
-                      )),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        onPressed: () => funimg(ImageSource.gallery),
-                        icon: const Icon(Ionicons.folder),
+          child: ListView(children: [
+            CircleAvatar(
+                radius: 60,
+                backgroundColor: Colors.grey,
+                backgroundImage:
+                    pickedimg == null ? null : FileImage(pickedimg!)),
+            const SizedBox(
+              height: 10,
+            ),
+            Column(
+              children: [
+                Text("Choose image From ?",
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                    )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: () => funimg(ImageSource.gallery),
+                      icon: const Icon(Ionicons.folder),
+                    ),
+                    IconButton(
+                      onPressed: () => funimg(ImageSource.camera),
+                      icon: const Icon(
+                        Ionicons.camera,
                       ),
-                      IconButton(
-                        onPressed: () => funimg(ImageSource.camera),
-                        icon: const Icon(
-                          Ionicons.camera,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.person_outline_outlined),
-                    labelText: 'Ad title',
-                    hintText: 'Enter Your Ad title',
-                    border: OutlineInputBorder()),
-                controller: titleController,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.person_outline_outlined),
-                    labelText: 'Brief',
-                    hintText: 'Enter a brief about job',
-                    border: OutlineInputBorder()),
-                controller: briefController,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.person_outline_outlined),
-                    labelText: 'Experience years',
-                    hintText: '0-3, 4-7, 8 and more',
-                    border: OutlineInputBorder()),
-                controller: yearController,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.person_outline_outlined),
-                    labelText: 'Requirments',
-                    hintText: 'Add requirments',
-                    border: OutlineInputBorder()),
-                controller: reqController,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.person_outline_outlined),
-                    labelText: 'Url',
-                    hintText: 'Add url to your course',
-                    border: OutlineInputBorder()),
-                controller: urlController,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              ElevatedButton(
-                style: OutlinedButton.styleFrom(
-                    backgroundColor: buttonColor,
-                    shape: RoundedRectangleBorder(),
-                    padding: EdgeInsets.symmetric(vertical: 15)),
-                onPressed: () async {
-                  final url;
-                  if (pickedimg == null) {
-                    url = widget.imageUrl!;
-                  } else {
-                    DateTime now = DateTime.now();
-                    final storageRef = FirebaseStorage.instance
-                        .ref()
-                        .child('Users_img')
-                        .child(comp_name.toString() +
-                            now.hour.toString() +
-                            now.minute.toString() +
-                            now.second.toString() +
-                            '.jpg');
-                    await storageRef.putFile(pickedimg!);
-                    url = await storageRef.getDownloadURL();
-                  }
-
-                  await FirebaseFirestore.instance
-                      .collection('Jobs')
-                      .doc(widget.docnid)
-                      .update({
-                    'job_title': titleController.text,
-                    'breif': briefController.text,
-                    'years': yearController.text,
-                    'requirements': reqController.text,
-                    'url': urlController.text,
-                    'img_url': url
-                  });
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  "Update",
-                  style: GoogleFonts.montserrat(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
+                    ),
+                  ],
                 ),
+              ],
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.person_outline_outlined),
+                  labelText: 'Ad Title',
+                  hintText: 'Edit Title name',
+                  border: OutlineInputBorder()),
+              controller: titleController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.location_pin),
+                  labelText: 'Brief',
+                  hintText: 'Edit Brief',
+                  border: OutlineInputBorder()),
+              controller: briefController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.calendar_month),
+                  labelText: 'Years',
+                  hintText: 'Edit Years',
+                  border: OutlineInputBorder()),
+              controller: yearsController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.book),
+                  labelText: 'Requiermints',
+                  hintText: 'Edit Requiermints',
+                  border: OutlineInputBorder()),
+              controller: reqController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.phone),
+                  labelText: 'URL',
+                  hintText: 'Edit URL',
+                  border: OutlineInputBorder()),
+              controller: urlController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: OutlinedButton.styleFrom(
+                  backgroundColor: buttonColor,
+                  shape: RoundedRectangleBorder(),
+                  padding: EdgeInsets.symmetric(vertical: 15)),
+              onPressed: () async {
+                final url;
+                if (pickedimg == null) {
+                  url = widget.imageUrl!;
+                } else {
+                  DateTime now = DateTime.now();
+                  final storageRef = FirebaseStorage.instance
+                      .ref()
+                      .child('Users_img')
+                      .child(comp_name.toString() +
+                          now.hour.toString() +
+                          now.minute.toString() +
+                          now.second.toString() +
+                          '.jpg');
+                  await storageRef.putFile(pickedimg!);
+                  url = await storageRef.getDownloadURL();
+                }
+                await FirebaseFirestore.instance
+                    .collection('Jobs')
+                    .doc(widget.docnid!)
+                    .update({
+                  'job_title': titleController.text,
+                  'breif': briefController.text,
+                  'years': yearsController.text,
+                  'requirements': reqController.text,
+                  'url': urlController.text,
+                  'img_url': url,
+                });
+                Navigator.pop(context);
+              },
+              child: Text(
+                "Update",
+                style: GoogleFonts.montserrat(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
               ),
-            ],
-          ),
+            ),
+          ]),
         ),
       ),
     );
--- lib/Screens/Company/updateTraining.dart
+++ lib/Screens/Company/updateTraining.dart
@@ -45,16 +45,6 @@ class _Update_trainingState extends State<Update_training> {
 
   @override
   Widget build(BuildContext context) {
-    FirebaseFirestore.instance
-        .collection('Users')
-        .doc(FirebaseAuth.instance.currentUser!.uid)
-        .snapshots()
-        .listen((event) {
-      setState(() {
-        comp_name = event['Fullname'];
-        img_url = event['img'];
-      });
-    });
     TextEditingController titleController =
         TextEditingController(text: widget.title == null ? null : widget.title);
     TextEditingController briefController =
@@ -63,6 +53,7 @@ class _Update_trainingState extends State<Update_training> {
         TextEditingController(text: widget.price == null ? null : widget.price);
     TextEditingController urlController =
         TextEditingController(text: widget.url == null ? null : widget.url);
+
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 75,
@@ -75,169 +66,135 @@ class _Update_trainingState extends State<Update_training> {
                 width: 26,
               ),
               onPressed: () {
-                Navigator.pushNamed(context, "companyHome");
+                Navigator.pop(context);
               }),
         ),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Welcome",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 153, 152, 152),
-                    fontSize: subTitleSize,
-                  ),
-                ),
-                Text(
-                  "$comp_name ,",
-                  style: const TextStyle(color: Colors.black),
-                )
-              ],
-            ),
-          ],
-        ),
-        centerTitle: true,
-        actions: [
-          CircleAvatar(
-            radius: 25,
-            backgroundColor: Colors.grey,
-            backgroundImage: NetworkImage(img_url),
-          ),
-          const SizedBox(
-            width: 15,
-          ),
-        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Form(
-          child: ListView(
-            children: [
-              CircleAvatar(
-                  radius: 60,
-                  backgroundColor: Colors.grey,
-                  backgroundImage:
-                      pickedimg == null ? null : FileImage(pickedimg!)),
-              const SizedBox(
-                height: 10,
-              ),
-              Column(
-                children: [
-                  Text("Choose image From ?",
-                      style: GoogleFonts.poppins(
-                        fontSize: 16,
-                      )),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        onPressed: () => funimg(ImageSource.gallery),
-                        icon: const Icon(Ionicons.folder),
+          child: ListView(children: [
+            CircleAvatar(
+                radius: 60,
+                backgroundColor: Colors.grey,
+                backgroundImage:
+                    pickedimg == null ? null : FileImage(pickedimg!)),
+            const SizedBox(
+              height: 10,
+            ),
+            Column(
+              children: [
+                Text("Choose image From ?",
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                    )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: () => funimg(ImageSource.gallery),
+                      icon: const Icon(Ionicons.folder),
+                    ),
+                    IconButton(
+                      onPressed: () => funimg(ImageSource.camera),
+                      icon: const Icon(
+                        Ionicons.camera,
                       ),
-                      IconButton(
-                        onPressed: () => funimg(ImageSource.camera),
-                        icon: const Icon(
-                          Ionicons.camera,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.person_outline_outlined),
-                    labelText: 'Ad title',
-                    hintText: 'Enter Your Ad title',
-                    border: OutlineInputBorder()),
-                controller: titleController,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.person_outline_outlined),
-                    labelText: 'Brief',
-                    hintText: 'Enter a brief about job',
-                    border: OutlineInputBorder()),
-                controller: briefController,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.person_outline_outlined),
-                    labelText: 'price',
-                    hintText: 'Add price',
-                    border: OutlineInputBorder()),
-                controller: priceController,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.person_outline_outlined),
-                    labelText: 'Url',
-                    hintText: 'Add url to your course',
-                    border: OutlineInputBorder()),
-                controller: urlController,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              ElevatedButton(
-                style: OutlinedButton.styleFrom(
-                    backgroundColor: buttonColor,
-                    shape: RoundedRectangleBorder(),
-                    padding: EdgeInsets.symmetric(vertical: 15)),
-                onPressed: () async {
-                  final url;
-                  if (pickedimg == null) {
-                    url = widget.imageUrl!;
-                  } else {
-                    DateTime now = DateTime.now();
-                    final storageRef = FirebaseStorage.instance
-                        .ref()
-                        .child('Users_img')
-                        .child(comp_name.toString() +
-                            now.hour.toString() +
-                            now.minute.toString() +
-                            now.second.toString() +
-                            '.jpg');
-                    await storageRef.putFile(pickedimg!);
-                    url = await storageRef.getDownloadURL();
-                  }
-
-                  await FirebaseFirestore.instance
-                      .collection('Training')
-                      .doc(widget.docnid)
-                      .update({
-                    'job_title': titleController.text,
-                    'breif': briefController.text,
-                    'price': priceController.text,
-                    'url': urlController.text
-                  });
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  "Update",
-                  style: GoogleFonts.montserrat(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
+                    ),
+                  ],
                 ),
+              ],
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.person_outline_outlined),
+                  labelText: 'Ad Title',
+                  hintText: 'Edit Title name',
+                  border: OutlineInputBorder()),
+              controller: titleController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.location_pin),
+                  labelText: 'Brief',
+                  hintText: 'Edit Brief',
+                  border: OutlineInputBorder()),
+              controller: briefController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.calendar_month),
+                  labelText: 'Price',
+                  hintText: 'Edit Price',
+                  border: OutlineInputBorder()),
+              controller: priceController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.phone),
+                  labelText: 'URL',
+                  hintText: 'Edit URL',
+                  border: OutlineInputBorder()),
+              controller: urlController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: OutlinedButton.styleFrom(
+                  backgroundColor: buttonColor,
+                  shape: RoundedRectangleBorder(),
+                  padding: EdgeInsets.symmetric(vertical: 15)),
+              onPressed: () async {
+                final url;
+                if (pickedimg == null) {
+                  url = widget.imageUrl!;
+                } else {
+                  DateTime now = DateTime.now();
+                  final storageRef = FirebaseStorage.instance
+                      .ref()
+                      .child('Users_img')
+                      .child(comp_name.toString() +
+                          now.hour.toString() +
+                          now.minute.toString() +
+                          now.second.toString() +
+                          '.jpg');
+                  await storageRef.putFile(pickedimg!);
+                  url = await storageRef.getDownloadURL();
+                }
+                await FirebaseFirestore.instance
+                    .collection('Training')
+                    .doc(widget.docnid!)
+                    .update({
+                  'job_title': titleController.text,
+                  'breif': briefController.text,
+                  'price': priceController.text,
+                  'url': urlController.text,
+                  'img_url': url,
+                });
+                Navigator.pop(context);
+              },
+              child: Text(
+                "Update",
+                style: GoogleFonts.montserrat(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
               ),
-            ],
-          ),
+            ),
+          ]),
         ),
       ),
     );
