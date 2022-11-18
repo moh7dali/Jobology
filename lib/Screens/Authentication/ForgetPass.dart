72cfc17c66b91ec2df469377e9736979feb47877
moh7dali <mohd96181@gmail.com>
2022-11-19 01:20:20 +0200

    update

--- lib/Screens/Authentication/ForgetPass.dart
+++ lib/Screens/Authentication/ForgetPass.dart
@@ -18,24 +18,6 @@ class _ForgetPasswordState extends State<ForgetPassword> {
           email: emailController.text.toLowerCase().trim());
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text("check your email")));
-      // showDialog(
-      //   context: context,
-      //   builder: (context) {
-      //     return AlertDialog(
-      //       content: Column(mainAxisAlignment: MainAxisAlignment.center,
-      //         children: [
-      //           Text("check your email"),
-      //           ElevatedButton(style: OutlinedButton.styleFrom(
-      //                   backgroundColor: buttonColor,
-      //                   shape: RoundedRectangleBorder(),
-      //                   padding: EdgeInsets.symmetric(vertical: 15)),onPressed: (){Navigator.popAndPushNamed(context, "Login");}, child: Text("Return to Login page",style: GoogleFonts.montserrat(
-      //                       fontSize: 15, fontWeight: FontWeight.bold))),
-      //         ],
-      //       ),
-
-      //     );
-      //   },
-      // );
     } on FirebaseAuthException catch (e) {
       print(e);
       ScaffoldMessenger.of(context)
@@ -52,10 +34,6 @@ class _ForgetPasswordState extends State<ForgetPassword> {
         child: SingleChildScrollView(
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            // Image.network(
-            //   'https://static.wixstatic.com/media/bcace9_c8332fc9319a4cb79cce54c1324673c7~mv2.png/v1/fill/w_370,h_104,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/JOBOLOGY%20LOGO%20FINAL.png',
-            //   height: height * 0.2,
-            // ),
             Text(
               "Forget Password ",
               style: GoogleFonts.montserrat(
